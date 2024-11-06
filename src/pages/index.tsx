698e5f17165ebc3a0a5f5f4cbb718c51641f4108
devRyan9025 <alex7006@naver.com>
2024-11-06 17:31:06 +0900

    refactor : 서버 사이드 렌더링(SSR) 적용하여 백엔드 서버 연결

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,42 @@
 import SearchableLayouyt from '@/components/searchable-layout';
 import style from './index.module.css';
 import { ReactNode } from 'react';
-import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
+import { InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
-export default function Home() {
+// SSR 설정하기 : 컴포넌트보다 먼저 실행되어서, 컴포넌트의 필요한  데이터를 불러오는 함수
+export const getServerSideProps = async () => {
+  // 병렬로 데이터 fetching 하기
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    props: {
+      allBooks,
+      recoBooks,
+    },
+  };
+};
+
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
